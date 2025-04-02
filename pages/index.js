c11eb1a352f0b7886dfd5bcdb4ab4007720dbe92
sukarim152 <umairkarim84@gmail.com>
2025-04-02 14:47:32 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ export default function Home() {
     <div>
       <h1>Welcome to Strata Management</h1>
       <p>Manage your building efficiently.</p>
+      <img src="/Umair Photo.jpg" alt="Umair Photo" width="300" />
     </div>
   );
 }
