b803c14973bd897214dc673f4a5478c73a03c85a
sukarim152 <umairkarim84@gmail.com>
2025-04-05 17:30:23 +1100

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+
 export default function About() {
   return (
     <div>
-      <h1>About Our Strata Building</h1>
-      <p>We are committed to excellent management and happy residents.</p>
+      {/* Navigation links at the top of the page */}
+      <nav>
+        <Link href="/">Home</Link> | 
+        <Link href="/about">About</Link> | 
+        <Link href="/services">Services</Link> | 
+        <Link href="/contact">Contact</Link>
+      </nav>
+
+      {/* Page content */}
+      <h1>About Our Strata Management</h1>
+      <p>We are committed to excellent management and providing a great experience for our residents. Learn more about our services and how we manage our properties effectively.</p>
     </div>
   );
 }
