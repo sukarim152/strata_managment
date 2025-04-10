2c33fa7a960654396dab6171d8fcbb4ea8296a69
sukarim152 <umairkarim84@gmail.com>
2025-04-10 21:33:40 +1000

    Create privacy.module.js

---
+++ styles/privacy.module.js
@@ -0,0 +1,11 @@
+/* pages/privacy.module.css */
+.container {
+  font-family: 'Arial', sans-serif;
+  background-color: #f7f7f7;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+h1 {
+  color: #333;
+}
