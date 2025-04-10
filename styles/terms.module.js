b0706958261400c78bc225f3d3b25d7e26681cf6
sukarim152 <umairkarim84@gmail.com>
2025-04-10 21:32:49 +1000

    Create terms.module.js

---
+++ styles/terms.module.js
@@ -0,0 +1,11 @@
+/* pages/terms.module.css */
+.container {
+  font-family: 'Verdana', sans-serif;
+  background-color: #fafafa;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+h1 {
+  color: #333;
+}
