82596aa3299d81ef232004bdb6ecd201f0e23ecf
sukarim152 <umairkarim84@gmail.com>
2025-04-10 22:05:06 +1000

    Create thank-you.js

---
+++ pages/thank-you.js
@@ -0,0 +1,8 @@
+export default function ThankYou() {
+  return (
+    <div>
+      <h1>Thank You!</h1>
+      <p>Your message has been received. We’ll get back to you shortly.</p>
+    </div>
+  );
+}
