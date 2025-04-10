5a5274528f4c6d57d5235eabbd81faca6c7e379e
sukarim152 <umairkarim84@gmail.com>
2025-04-10 21:28:04 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from './about.module.css';
+import styles from '../styles/about.module.css'; // Import the CSS module
 
 export default function About() {
   return (
